8633313fc299446cfd2fb1c101ef16a726016c54
Josh Thornton <josh@speedscale.com>
2025-04-12 22:47:19 -0400

    kv and test

---
+++ kv.go
@@ -0,0 +1,96 @@
+package main
+
+type operation int
+
+const (
+	opUnknown operation = iota
+	opStore
+	opLoad
+)
+
+type kvEvent struct {
+	op operation
+	k  string
+	v  string
+	ok bool
+}
+
+// KVStore is a thread safe wrapper of kvStore that uses an Erlang-inspired
+// model for allowing concurrent operations by passing messages to a synchronous
+// process.  This is much like the actor model, but a contrived implementation
+// which is intended to more closely model (my limited understanding of)
+// Erlang's behind-the-scenes handling of behaviors.
+//
+// Ideas inspired from
+// https://stevana.github.io/erlangs_not_about_lightweight_processes_and_message_passing.html
+type KVStore struct {
+	inner *kvStore
+	in    chan kvEvent
+	out   chan kvEvent
+}
+
+func NewKVStore() *KVStore {
+	kvstore := &KVStore{
+		inner: newkvStore(),
+		in:    make(chan kvEvent),
+		out:   make(chan kvEvent),
+	}
+
+	go func() {
+		for event := range kvstore.in {
+			switch event.op {
+			case opUnknown:
+				panic("we don't know")
+			case opStore:
+				kvstore.inner.Store(event.k, event.v)
+				kvstore.out <- kvEvent{}
+			case opLoad:
+				v, ok := kvstore.inner.Load(event.k)
+				kvstore.out <- kvEvent{
+					v:  v,
+					ok: ok,
+				}
+			}
+		}
+	}()
+
+	return kvstore
+}
+
+func (s *KVStore) Store(k, v string) {
+	s.in <- kvEvent{
+		op: opStore,
+		k:  k,
+		v:  v,
+	}
+	<-s.out
+}
+
+func (s *KVStore) Load(k string) (string, bool) {
+	s.in <- kvEvent{
+		op: opLoad,
+		k:  k,
+	}
+	res := <-s.out
+	return res.v, res.ok
+}
+
+// kvStore stores and loads key / value pairs.  It is not thread safe.
+type kvStore struct {
+	data map[string]string
+}
+
+func newkvStore() *kvStore {
+	return &kvStore{
+		data: make(map[string]string),
+	}
+}
+
+func (s *kvStore) Store(k, v string) {
+	s.data[k] = v
+}
+
+func (s *kvStore) Load(k string) (string, bool) {
+	v, ok := s.data[k]
+	return v, ok
+}
---
+++ kv_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestKVStore(t *testing.T) {
+	kv := NewKVStore()
+
+	// create some concurrency that will fail when the race detector runs if not
+	// handled properly
+	var wg sync.WaitGroup
+	for range 10 {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			kv.Store("foo", "bar")
+			kv.Store("baz", "bam")
+			kv.Load("foo")
+		}()
+	}
+
+	wg.Wait()
+
+	v, ok := kv.Load("foo")
+	require.True(t, ok)
+	require.Equal(t, "bar", v)
+
+	_, ok = kv.Load("not-found")
+	require.False(t, ok)
+
+	v, ok = kv.Load("baz")
+	require.True(t, ok)
+	require.Equal(t, "bam", v)
+}
